26f9d0d31a2b526fee2708a4fdf87edd3c430697
Chris Northwood <chris@pling.org.uk>
2012-02-18 12:02:12 +0000

    Add on board image

--- generate.sh
+++ generate.sh
@@ -76,6 +76,7 @@ $CONTENT
                 <br/><img src="img/olympics.jpg" alt="Living the Olympic and Paralympic values" />
                 <img src="img/lppa.jpg" alt="Leading Parent Partnership Award" />
                 <img src="img/nationalcollegeforschoolleadership.jpg" alt="National College for School Leadership" />
+                <a href="http://www.travelsouthyorkshire.com/onboard/"><img src="img/onboard.jpg" alt="New for schools Everyone's on board Are you? Try it now" /></a>
                 </p>
 		<p><span>&copy; 2008-2012 <a href="http://www.pling.org.uk/">Chris Northwood</a></span><br />
 		<a href="http://andreasviklund.com/templates/" title="Original CSS template design">Original design</a> by <a href="http://andreasviklund.com/" title="Andreas Viklund">Andreas Viklund</a></p>
