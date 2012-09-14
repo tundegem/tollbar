726895647c07810010b8f83e65109399758c77f6
Chris Northwood <chris@pling.org.uk>
2012-09-14 19:10:15 +0000

    Fix https in generate.sh

--- generate.sh
+++ generate.sh
@@ -12,7 +12,7 @@ done
 
 for file in `find $SOURCEDIR -name \*.tmpl` ; do
 	. $file
-	if [ "`echo $LOCATION | cut -d: -f1`" != "http" ] ; then
+	if [ "`echo $LOCATION | head -c4`" != "http" ] ; then
 		cat >$OUTPUTDIR/$LOCATION <<EOF
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
